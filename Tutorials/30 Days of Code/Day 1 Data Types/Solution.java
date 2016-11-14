c7482a8aa6f4073a96ae03c874a8de36ed08bdf3
Deepak <pdeepak93@gmail.com>
2016-11-14 11:35:17 +0530

    Day 1 Challenge - 11-14-2016

---
+++ Tutorials/30 Days of Code/Day 1 Data Types/Solution.java
@@ -0,0 +1,20 @@
+        /* Declare second integer, double, and String variables. */
+
+        int intVar=scan.nextInt();
+        double doubleVar=scan.nextDouble();
+        scan.nextLine();
+        String stringVar =scan.nextLine();
+
+        System.out.println(i+intVar);
+        System.out.println(d+doubleVar);
+        System.out.println(s+stringVar);
+
+
+        /* Read and save an integer, double, and String to your variables.*/
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
